9069718d51767088e0a26155d43938763c7665b0
hujibing <1401818586@qq.com>
2019-07-16 14:41:08 +0800

    first commit

---
+++ my-project/src/components/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+   <h1>关于</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  data () {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
